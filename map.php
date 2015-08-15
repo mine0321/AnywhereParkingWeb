7cba8983e57ee8c952d7e591b474a64973500215
長峯 洸弥 <nagamine@yairilab.net>
2015-08-15 11:11:01 +0000

    サイズの変更

--- map.php
+++ map.php
@@ -1,6 +1,6 @@
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 
-<div id="map" style="width: 400px; height: 280px;"></div>
+<div id="map" style="width:100%; height:100%></div>
 <script type="text/javascript">
 // 地図
 var map = new google.maps.Map(document.getElementById("map"), {
@@ -15,8 +15,8 @@ new google.maps.Marker({
   position: new google.maps.LatLng(35.710139, 139.810833),
   map: map,
   icon: new google.maps.MarkerImage(
-    "images/car.png"                     // url
-    //new google.maps.Size(31,31), // size
+    "images/car.png",                     // url
+    new google.maps.Size(100,100), // size
     //new google.maps.Point(0,0),  // origin
     //new google.maps.Point(16,16) // anchor
   )
