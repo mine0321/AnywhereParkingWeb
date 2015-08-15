d9db1bed8a4049c5bb81d9d10240d2237ee3d758
長峯 洸弥 <nagamine@yairilab.net>
2015-08-15 10:49:37 +0000

    戻す

--- map.php
+++ map.php
@@ -1,4 +1,3 @@
-
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 
 <div id="map" style="width: 400px; height: 280px;"></div>
@@ -13,13 +12,8 @@ var map = new google.maps.Map(document.getElementById("map"), {
 
 // マーカー
 new google.maps.Marker({
-  position:   new google.maps.LatLng(35.710139, 139.810833),
+  position: new google.maps.LatLng(35.710139, 139.810833),
   map: map,
-  icon: new google.maps.MarkerImage(
-    'images/car.png',                     // url
-    new google.maps.Size(31,31), // size
-    //new google.maps.Point(0,0),  // origin
-    //new google.maps.Point(16,16) // anchor
-  )
+  icon: "images/car.png"
 });
 </script>
