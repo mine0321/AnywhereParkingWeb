b713a8e1b75d2be595562ff49d7f233f96719592
長峯 洸弥 <nagamine@yairilab.net>
2015-08-15 10:11:51 +0000

    地図を追加

--- map.php
+++ map.php
@@ -1,3 +1,19 @@
-echo '<script type="text/javascript">
-          $("#button-option").hide();
-      </script>';
+echo '<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+
+<div id="map" style="width: 400px; height: 280px;"></div>
+<script type="text/javascript">
+// 地図
+var map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 14,
+  center: new google.maps.LatLng(35.710139, 139.810833),
+  scrollwheel: false,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+// マーカー
+new google.maps.Marker({
+  position: new google.maps.LatLng(35.710139, 139.810833),
+  map: map,
+  icon: "v.png"
+});
+</script>';
