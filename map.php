d8aae974794eb83c499247434167d9a237c2cc41
長峯 洸弥 <nagamine@yairilab.net>
2015-08-15 10:32:23 +0000

    マーカーサイズの変更

--- map.php
+++ map.php
@@ -1,4 +1,5 @@
-echo '<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 
 <div id="map" style="width: 400px; height: 280px;"></div>
 <script type="text/javascript">
@@ -12,8 +13,13 @@ var map = new google.maps.Map(document.getElementById("map"), {
 
 // マーカー
 new google.maps.Marker({
-  position: new google.maps.LatLng(35.710139, 139.810833),
+  position:   new google.maps.LatLng(35.710139, 139.810833),
   map: map,
-  icon: "images/car.png"
+  icon: new google.maps.MarkerImage(
+    'images/car.png',                     // url
+    new google.maps.Size(31,31), // size
+    //new google.maps.Point(0,0),  // origin
+    //new google.maps.Point(16,16) // anchor
+  )
 });
-</script>';
+</script>
