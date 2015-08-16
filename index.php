520d737f1e94729cb0e4bf1dbcb2fe5cd72b949a
直喜 友野 <tnaokit.tomono@gmail.com>
2015-08-16 06:24:48 +0000

    ok

--- index.php
+++ index.php
@@ -26,7 +26,29 @@
 				</footer>
 			</section>
 
+<script type="text/javascript">
+var userdata;
+var wsUrl = '<http://anywhereparkingnodered.mybluemix.net/ws/sensor>';
+function connect() {
+  socket = new WebSocket(wsUrl);
+  socket.onmessage = function(e) {
+    var sensorData = JSON.parse(e.data);
 
+    // Update temperature data
+    //temperatureData.setValue(0, 1, sensorData.temp);
+    //temperatureData.setValue(1, 1, sensorData.objectTemp);
+    userdata.setValue(sensorData.lat)
+    var userdata
+
+    // Update humidity data
+    //humidityData.setValue(0, 1, sensorData.humidity);
+
+    //temperatureChart.draw(temperatureData, temperatureOptions);
+    //humidityChart.draw(humidityData, humidityOptions);
+  };
+};
+
+</script>
 
 	</body>
 </html>
