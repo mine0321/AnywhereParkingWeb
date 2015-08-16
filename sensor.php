ba9b2d195c599c9c7d31db453bda435b82cc5ae3
直喜 友野 <tnaokit.tomono@gmail.com>
2015-08-16 06:35:57 +0000

    ok

---
+++ sensor.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<!---->
+<html>
+<head>
+<title>AnywhereParking</title>
+<meta charset="utf-8" />
+
+</head>
+<body>
+<script type="text/javascript">
+var userdata;
+var wsUrl = '<http://anywhereparkingnodered.mybluemix.net/ws/sensor>';
+function connect() {
+  socket = new WebSocket(wsUrl);
+  socket.onmessage = function(e) {
+    var sensorData = JSON.parse(e.data);
+
+    // Update temperature data
+    //temperatureData.setValue(0, 1, sensorData.temp);
+    //temperatureData.setValue(1, 1, sensorData.objectTemp);
+    userdata.setValue(sensorData.lat)
+    alert(userdata)
+    <p>#############</p>
+    alert(sensordata)
+    // Update humidity data
+    //humidityData.setValue(0, 1, sensorData.humidity);
+
+    //temperatureChart.draw(temperatureData, temperatureOptions);
+    //humidityChart.draw(humidityData, humidityOptions);
+  };
+};
+
+</script>
+
+</body>
+</html>
